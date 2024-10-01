36a08f4af1ebca11e957951a41b19e1cfe89a046
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-10-01 18:01:57 +0900

    revert to old version

--- cldm/warping_cldm_network.py
+++ cldm/warping_cldm_network.py
@@ -11,7 +11,7 @@ import torch
 import torch as th
 import torch.nn as nn
 from cleanfid import fid
-from pytorch_lightning.utilities.rank_zero import rank_zero_only
+from pytorch_lightning.utilities.distributed import rank_zero_only
 
 import torchvision.transforms as T
 import torch.nn.functional as F
